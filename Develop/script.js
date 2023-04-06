dde2e2f7ac515a2c8896a03afd41021d8b5526c1
suzakijun1 <dyong1997@gmail.com>
2023-04-05 21:25:34 -0400

    first

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
+var submitButton = document.querySelector(".saveBtn");
+var inputInformation = document.querySelector(".description");
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var time = dayjs().format("hh:mm:ss");
+//$("#currentDay").text(time);
+var unix = dayjs().unix;
+$("currentDay").text(unix);
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -16,6 +24,16 @@ $(function () {
   // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
+
+  submitButton.addEventListener("click", function () {
+    var moreInformation = inputInformation.value;
+    if (moreInformation === null) {
+      console.log("no value entered!");
+    } else {
+      console.log(moreInformation);
+    }
+  });
+
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
